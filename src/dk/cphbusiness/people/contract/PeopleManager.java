64a0271c74a0aa7b68bc6382bd286bf62473c1ff
Anders Kalhauge <anders@kalhauge.dk>
2014-11-27 11:32:56 +0100

    Just for fun

--- src/dk/cphbusiness/people/contract/PeopleManager.java
+++ src/dk/cphbusiness/people/contract/PeopleManager.java
@@ -5,7 +5,10 @@ import dk.cphbusiness.people.contract.dto.PersonIdentifier;
 import dk.cphbusiness.people.contract.dto.PersonSummary;
 import java.util.Collection;
 import javax.ejb.Remote;
-
+/**
+ * This is just plain stupid
+ * @author anders
+ */
 @Remote
 public interface PeopleManager {
   Collection<PersonSummary> listPeople();
